2ccc013df13a9b9a11c446f7ce3906e9bafe55e0
Miquel Costa <mmiiqquueell@hotmail.com>
2017-02-28 17:29:19 +0100

    REV: V 0.27
    * Añadidas todas las opciones al menú principal.
    * Añadido tutoriales en iFrame.
    * Añadidos ejercicios de clase en PDF a modo de tutorial.
    * Añadido archivo JavaScript.
    * Añadido archivo JQuery.
    * Corregida la estructura de carpetas.
    * Corregido un error del menú principal.
    * Corregido un error en los iFrames.
    * Corregido un error de tamaño de iFrames.
    * Corregido un error de carga y descarga de iFrames.
    * Corregidos algunos IDs y CLASSs

---
+++ WebContent/js/Script.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+	var altura = $('.cssmenu').offset().top;
+	
+	$(window).on('scroll', function(){
+		if ( $(window).scrollTop() > altura ){
+			$('.cssmenu').addClass('cssmenu-fixed');
+		} else {
+			$('.cssmenu').removeClass('cssmenu-fixed');
+		}
+	});
+ 
+});
+
+
+function mostrar1(){
+document.getElementById('glinks').style.display = 'none';
+document.getElementById('tgramas').style.display = 'block';
+document.getElementById('tgrevertreset').style.display = 'none';
+document.getElementById('tgeclipse').style.display = 'none';
+document.getElementById('active1').style.backgroundColor = '#F73';
+document.getElementById('active2').style.backgroundColor = '';
+document.getElementById('active3').style.backgroundColor = '';
+}
+
+function mostrar2(){
+document.getElementById('glinks').style.display = 'none';
+document.getElementById('tgramas').style.display = 'none';
+document.getElementById('tgrevertreset').style.display = 'block';
+document.getElementById('tgeclipse').style.display = 'none';
+document.getElementById('active1').style.backgroundColor = '';
+document.getElementById('active2').style.backgroundColor = '#F73';
+document.getElementById('active3').style.backgroundColor = '';
+}
+
+function mostrar3(){
+document.getElementById('glinks').style.display = 'none';
+document.getElementById('tgramas').style.display = 'none';
+document.getElementById('tgrevertreset').style.display = 'none';
+document.getElementById('tgeclipse').style.display = 'block';
+document.getElementById('active1').style.backgroundColor = '';
+document.getElementById('active2').style.backgroundColor = '';
+document.getElementById('active3').style.backgroundColor = '#F73';
+}
+
