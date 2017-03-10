4d42ba1fddd4e6dc047e4aca68bb64d74de0e647
Miquel Costa <mmiiqquueell@hotmail.com>
2017-03-10 16:54:55 +0100

    REV: V 1.00
    * Añadido responsive completo.
    * Añadido menú responsive.
    * Solucionado problemas de redireccionamiento.
    * Solucionado problemas de JavaScript.
    - Algunos errores siguen presentes.

--- WebContent/js/Script.js
+++ WebContent/js/Script.js
@@ -8,6 +8,25 @@ $(document).ready(function(){
 			$('.cssmenu').removeClass('cssmenu-fixed');
 		}
 	});
+
+
+var URLhash = window.location.hash;
+if (URLhash == "#tgramas") {mostrar1();}
+else if (URLhash == "#tgrevertreset") {mostrar2();}
+else if (URLhash == "#tgeclipse") {mostrar3();}
+
+});
+
+$(document).ready(function(){
+	var altura = $('.cssmenuresponsive').offset().top;
+	
+	$(window).on('scroll', function(){
+		if ( $(window).scrollTop() > 125 ){
+			$('.cssmenuresponsive').addClass('cssmenuresponsive-fixed');
+		} else {
+			$('.cssmenuresponsive').removeClass('cssmenuresponsive-fixed');
+		}
+	});
  
 });
 
@@ -43,3 +62,36 @@ document.getElementById('active2').style.backgroundColor = '';
 document.getElementById('active3').style.backgroundColor = '#F73';
 }
 
+
+var i = 0;
+var menu = 0;
+var elems = document.getElementsByClassName('cssmenuresponsive');
+
+setInterval(function(){ 
+var w = window.innerWidth;
+if (w >= 501 && menu == 1 && w <= 640) {desp(); document.getElementById('cabeza').style.height = "103px";}
+else if (w >= 501 && menu == 0 && w <= 640) {document.getElementById('cabeza').style.height = "103px";}
+else if (w >= 641 && menu == 1) {desp(); document.getElementById('cabeza').style.height = "55px";}
+else if (w >= 641 && menu == 0) {document.getElementById('cabeza').style.height = "55px";}
+else if (w <= 500 && menu == 0) {document.getElementById('cabeza').style.height = "0px";}
+ }, 1000);
+
+
+
+function desp(){
+
+	if (menu == 0) {
+		for(i = 0; i < elems.length; i++) {
+			elems[i].style.display = "block";
+		}
+		menu = 1;
+		document.getElementById('cabeza').style.height = "199px";
+	}
+	else {
+		for(i = 0; i < elems.length; i++) {
+			elems[i].style.display = "none";
+		}
+		document.getElementById('cabeza').style.height = "0px";
+		menu = 0;
+	}
+}
